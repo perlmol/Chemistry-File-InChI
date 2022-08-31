6a1adae8e68a502800db0343ec07874320148fce
Andrius Merkys <andrius.merkys@gmail.com>
2022-08-31 09:21:45 +0300

    Adding simple parser interface and a test with InChI strings from https://github.com/metamolecular/inchi-grammar/blob/master/strings.txt.

--- lib/Chemistry/File/InChI.pm
+++ lib/Chemistry/File/InChI.pm
@@ -3,7 +3,17 @@ package Chemistry::File::InChI;
 use strict;
 use warnings;
 
+use Chemistry::File::InChI::Parser;
+
 # ABSTRACT: InChI format reader
 # VERSION
+
+sub parse
+{
+    my( $string ) = @_;
+
+    my $parser = Chemistry::File::InChI::Parser->new;
+    $parser->parse( $string );
+}
 
 1;
