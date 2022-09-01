f9e69af30a6b4b1234601bb81d5d56b326ee56a9
Andrius Merkys <andrius.merkys@gmail.com>
2022-09-01 08:52:30 +0300

    Chemistry::File::InChI is derived from Chemistry::File.

--- lib/Chemistry/File/InChI.pm
+++ lib/Chemistry/File/InChI.pm
@@ -3,17 +3,31 @@ package Chemistry::File::InChI;
 use strict;
 use warnings;
 
+use base 'Chemistry::File';
+
 use Chemistry::File::InChI::Parser;
 
 # ABSTRACT: InChI format reader
 # VERSION
 
+Chemistry::Mol->register_format(inchi => __PACKAGE__);
+
 sub parse
 {
-    my( $string ) = @_;
+    my ($string) = @_;
 
     my $parser = Chemistry::File::InChI::Parser->new;
     return $parser->parse( $string );
+}
+
+sub name_is {
+    my ($self, $fname) = @_;
+    $fname =~ /\.inchi$/i;
+}
+
+sub file_is {
+    my ($self, $fname) = @_;
+    $fname =~ /\.inchi$/i;
 }
 
 1;
